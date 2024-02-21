49dce5df0c8b6188fdcb4d3ddcf9ee0ec2e47a93
Vitaliy <mitradis@gmail.com>
2024-02-21 05:55:26 +0300

    Merge

---
+++ Clear.ps1
@@ -0,0 +1,85 @@
+$Host.UI.RawUI.WindowTitle = 'Remove all revisions except Pro'
+if ([System.IO.File]::Exists("Z:\EN.txt"))
+{
+	dism /delete-image /imagefile:Z:\install.wim /index:10
+	dism /delete-image /imagefile:Z:\install.wim /index:9
+	dism /delete-image /imagefile:Z:\install.wim /index:8
+	dism /delete-image /imagefile:Z:\install.wim /index:7
+	dism /delete-image /imagefile:Z:\install.wim /index:6
+	dism /delete-image /imagefile:Z:\install.wim /index:4
+	dism /delete-image /imagefile:Z:\install.wim /index:3
+	dism /delete-image /imagefile:Z:\install.wim /index:2
+	dism /delete-image /imagefile:Z:\install.wim /index:1
+}
+else
+{
+	dism /delete-image /imagefile:Z:\install.wim /index:5
+	dism /delete-image /imagefile:Z:\install.wim /index:4
+	dism /delete-image /imagefile:Z:\install.wim /index:2
+	dism /delete-image /imagefile:Z:\install.wim /index:1
+}
+$Host.UI.RawUI.WindowTitle = 'Mounting install.wim'
+mkdir Z:\Install
+dism /mount-image /imagefile:Z:\install.wim /index:1 /mountdir:Z:\Install
+$Host.UI.RawUI.WindowTitle = 'Adding a response file'
+mkdir Z:\Install\Windows\Panther
+move Z:\unattend.xml Z:\Install\Windows\Panther
+$Host.UI.RawUI.WindowTitle = 'Removing capability'
+dism /image:Z:\Install /remove-capability /capabilityname:App.StepsRecorder~~~~0.0.1.0
+dism /image:Z:\Install /remove-capability /capabilityname:Hello.Face.20134~~~~0.0.1.0
+dism /image:Z:\Install /remove-capability /capabilityname:MathRecognizer~~~~0.0.1.0
+dism /image:Z:\Install /remove-capability /capabilityname:OneCoreUAP.OneSync~~~~0.0.1.0
+$letters=@("D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+foreach ($letter in $letters) {
+	if ([System.IO.File]::Exists($letter+":\sources\install.wim"))
+	{
+		$found=$letter+":\sources\sxs\"
+	}
+}
+$Host.UI.RawUI.WindowTitle = 'Installation .NET Framework 3.5'
+dism /image:Z:\Install /enable-feature /featurename:NetFx3 /all /source:$found /limitaccess
+$Host.UI.RawUI.WindowTitle = 'Activation Direct Play'
+dism /image:Z:\Install /enable-feature /featurename:DirectPlay /all /source:$found /limitaccess
+$Host.UI.RawUI.WindowTitle = 'Disable Defender Definitions'
+dism /image:Z:\Install /disable-feature /featurename:Windows-Defender-Default-Definitions
+$Host.UI.RawUI.WindowTitle = 'Removing Pre-Installation Packages'
+$apps=@(
+"Clipchamp.Clipchamp_2.2.8.0_neutral_~_yxz26nhyzhsrt",
+"Microsoft.549981C3F5F10_3.2204.14815.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.BingNews_4.2.27001.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.BingWeather_4.53.33420.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.GamingApp_2021.427.138.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.GetHelp_10.2201.421.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.Getstarted_2021.2204.1.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.MicrosoftOfficeHub_18.2204.1141.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.MicrosoftSolitaireCollection_4.12.3171.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.MicrosoftStickyNotes_4.2.2.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.People_2020.901.1724.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.PowerAutomateDesktop_10.0.3735.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.StorePurchaseApp_12008.1001.113.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.Todos_2.54.42772.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.Windows.Photos_21.21030.25003.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.WindowsAlarms_2022.2202.24.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.WindowsCalculator_2020.2103.8.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.WindowsCamera_2022.2201.4.0_neutral_~_8wekyb3d8bbwe",
+"microsoft.windowscommunicationsapps_16005.14326.20544.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.WindowsFeedbackHub_2022.106.2230.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.WindowsMaps_2022.2202.6.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.WindowsNotepad_11.2112.32.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.WindowsSoundRecorder_2021.2103.28.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.WindowsStore_22204.1400.4.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.Xbox.TCUI_1.23.28004.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.XboxGameOverlay_1.47.2385.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.XboxGamingOverlay_2.622.3232.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.XboxIdentityProvider_12.50.6001.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.XboxSpeechToTextOverlay_1.17.29001.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.YourPhone_1.22022.147.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.ZuneMusic_11.2202.46.0_neutral_~_8wekyb3d8bbwe",
+"Microsoft.ZuneVideo_2019.22020.10021.0_neutral_~_8wekyb3d8bbwe",
+"MicrosoftCorporationII.QuickAssist_2022.414.1758.0_neutral_~_8wekyb3d8bbwe",
+"MicrosoftWindows.Client.WebExperience_421.20070.195.0_neutral_~_cw5n1h2txyewy"
+)
+foreach ($app in $apps) {
+	$Host.UI.RawUI.WindowTitle = 'Remove package: ' + $app
+	dism /image:Z:\Install /remove-provisionedappxpackage /packagename:$app
+}
---
+++ _Clear.bat
@@ -0,0 +1,192 @@
+mode con:cols=50 lines=1
+title Start work...
+
+call :Clear>>Z:\Clear.log 2>&1
+EXIT /b 0
+
+:Clear
+title Mount boot.wim
+mkdir Z:\boot
+dism /mount-image /imagefile:Z:\boot.wim /index:2 /mountdir:Z:\boot
+title Load registry
+reg load HKEY_LOCAL_MACHINE\WIM_BOOT Z:\boot\Windows\System32\config\SYSTEM
+TIMEOUT /T 1 /NOBREAK >nul
+title Disable TPM check
+reg add "HKEY_LOCAL_MACHINE\WIM_BOOT\Setup\LabConfig" /v BypassCPUCheck /t REG_DWORD /d 1
+reg add "HKEY_LOCAL_MACHINE\WIM_BOOT\Setup\LabConfig" /v BypassTPMCheck /t REG_DWORD /d 1
+reg add "HKEY_LOCAL_MACHINE\WIM_BOOT\Setup\LabConfig" /v BypassRAMCheck /t REG_DWORD /d 1
+reg add "HKEY_LOCAL_MACHINE\WIM_BOOT\Setup\LabConfig" /v BypassSecureBootCheck /t REG_DWORD /d 1
+TIMEOUT /T 1 /NOBREAK >nul
+title Unload registry
+reg unload HKEY_LOCAL_MACHINE\WIM_BOOT
+TIMEOUT /T 1 /NOBREAK >nul
+title Unmount boot.wim
+dism /unmount-wim /mountdir:Z:\boot /commit
+TIMEOUT /T 1 /NOBREAK >nul
+title Compress boot.wim
+if exist Z:\boot.wim (
+	Z:\WimOptimize.exe Z:\boot.wim
+) else (
+	del /f /q Z:\boot.wim
+)
+title Applying Clear.ps1
+dism /get-imageinfo /imagefile:Z:\install.wim /index:10
+if %ERRORLEVEL% EQU -1051328239 (
+	del /f /q Z:\EN.txt
+)
+%windir%\System32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy bypass -File Z:\Clear.ps1
+del /f /q Z:\EN.txt
+del /f /q Z:\Clear.ps1
+title Load registry
+reg load HKEY_LOCAL_MACHINE\WIM_SOFTWARE Z:\Install\Windows\System32\config\SOFTWARE
+reg load HKEY_LOCAL_MACHINE\WIM_SYSTEM Z:\Install\Windows\System32\config\SYSTEM
+reg load HKEY_LOCAL_MACHINE\WIM_CURRENT_USER Z:\Install\Users\Default\NTUSER.DAT
+TIMEOUT /T 1 /NOBREAK >nul
+title Applying Clear.reg
+reg import Z:\Clear.reg
+del /f /q Z:\Clear.reg
+title Disable Secondary Logs
+for /f "tokens=*" %%a in ('reg QUERY "HKEY_LOCAL_MACHINE\WIM_SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels"') do (
+	reg add "%%a" /v Enabled /t REG_DWORD /d 0 /f
+)
+TIMEOUT /T 1 /NOBREAK >nul
+title Unload registry
+reg unload HKEY_LOCAL_MACHINE\WIM_CURRENT_USER
+reg unload HKEY_LOCAL_MACHINE\WIM_SYSTEM
+reg unload HKEY_LOCAL_MACHINE\WIM_SOFTWARE
+title Hide NTUSER.DAT
+ATTRIB Z:\Install\Users\Default\NTUSER.DAT +S +H
+title Shortcuts
+move "Z:\Install\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\System Tools\Character Map.lnk" "Z:\Install\ProgramData\Microsoft\Windows\Start Menu\Programs\System Tools"
+set DEL="Z:\Install\ProgramData\Microsoft\Windows\Start Menu\Programs\System Tools\desktop.ini"
+type %DEL%>>%DEL%.temp
+del /f /q /a:sh %DEL%
+move %DEL%.temp %DEL%
+echo Character Map.lnk=@%SystemRoot%\system32\shell32.dll,-22021>>%DEL%
+ATTRIB %DEL% +S +H
+rd /s /q "Z:\Install\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\System Tools"
+rd /s /q "Z:\Install\ProgramData\Microsoft\Windows\Start Menu\Programs\Windows PowerShell"
+rd /s /q "Z:\Install\ProgramData\Microsoft\Windows\Start Menu\Programs\Maintenance"
+rd /s /q "Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell"
+rd /s /q "Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Maintenance"
+set DEL="Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\desktop.ini"
+type %DEL%>>%DEL%.temp
+del /f /q /a:sh %DEL%
+move %DEL%.temp %DEL%
+echo Administrative Tools.lnk=@%SystemRoot%\system32\shell32.dll,-21762>>%DEL%
+echo File Explorer.lnk=@%SystemRoot%\system32\shell32.dll,-22067>>%DEL%
+ATTRIB %DEL% +S +H
+set DEL="Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\desktop.ini"
+ATTRIB %DEL% -S -H
+del /f /q %DEL%
+move "Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk" "Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\Administrative Tools.lnk"
+move "Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk" "Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\File Explorer.lnk"
+attrib +r -s -h "Z:\Install\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories" /S /D
+title EdgeUpdate
+rd /s /q "Z:\Install\Program Files (x86)\Microsoft\EdgeUpdate"
+rd /s /q "Z:\Install\Program Files (x86)\Microsoft\EdgeCore\100.0.1185.36"
+rd /s /q "Z:\Install\Program Files (x86)\Microsoft\EdgeWebView\Application\100.0.1185.36"
+rd /s /q "Z:\Install\Program Files (x86)\Microsoft\Edge\Application\100.0.1185.36\Installer"
+del /f /q "Z:\Install\Program Files (x86)\Microsoft\Edge\Application\100.0.1185.36\elevation_service.exe"
+del /f /q "Z:\Install\Program Files (x86)\Microsoft\Edge\Application\100.0.1185.36\notification_helper.exe"
+del /f /q "Z:\Install\Program Files (x86)\Microsoft\Edge\Application\100.0.1185.36\notification_helper.exe.manifest"
+title Disable Appx Protect
+%windir%\System32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy remotesigned -Command "& Get-Acl -Path Z:\Install\Windows | Set-Acl -Path 'Z:\Install\Program Files\WindowsApps'"
+title OneDrive
+takeown /f Z:\Install\Windows\WinSxS\amd64_microsoft-windows-onedrive-setup_31bf3856ad364e35_10.0.22621.1_none_86d60ce019ce7baf
+icacls Z:\Install\Windows\WinSxS\amd64_microsoft-windows-onedrive-setup_31bf3856ad364e35_10.0.22621.1_none_86d60ce019ce7baf /grant "%username%":f /c /l /q
+rd /s /q Z:\Install\Windows\WinSxS\amd64_microsoft-windows-onedrive-setup_31bf3856ad364e35_10.0.22621.1_none_86d60ce019ce7baf
+set DELETELIST=Z:\Install\Windows\System32\OneDriveSetup.exe Z:\Install\Windows\WinSxS\Manifests\amd64_microsoft-windows-onedrive-setup_31bf3856ad364e35_10.0.22621.1_none_86d60ce019ce7baf.manifest Z:\Install\Windows\WinSxS\SettingsManifests\amd64_microsoft-windows-onedrive-setup_31bf3856ad364e35_10.0.22621.1_none_86d60ce019ce7baf.manifest Z:\Install\Windows\WinSxS\Manifests\amd64_microsoft-windows-onedrive-setupregistry_31bf3856ad364e35_10.0.22621.1_none_51812954380458e2.manifest Z:\Install\Windows\WinSxS\Manifests\wow64_microsoft-windows-onedrive-setupregistry_31bf3856ad364e35_10.0.22621.1_none_5bd5d3a66c651add.manifest Z:\Install\Windows\servicing\Packages\Microsoft-Windows-OneDrive-Setup-Package~31bf3856ad364e35~amd64~~10.0.22621.1.cat Z:\Install\Windows\servicing\Packages\Microsoft-Windows-OneDrive-Setup-Package~31bf3856ad364e35~amd64~~10.0.22621.1.mum Z:\Install\Windows\servicing\Packages\Microsoft-Windows-OneDrive-Setup-Package~31bf3856ad364e35~amd64~ru-RU~10.0.22621.1.cat Z:\Install\Windows\servicing\Packages\Microsoft-Windows-OneDrive-Setup-Package~31bf3856ad364e35~amd64~ru-RU~10.0.22621.1.mum Z:\Install\Windows\servicing\Packages\Microsoft-Windows-OneDrive-Setup-WOW64-Package~31bf3856ad364e35~amd64~~10.0.22621.1.cat Z:\Install\Windows\servicing\Packages\Microsoft-Windows-OneDrive-Setup-WOW64-Package~31bf3856ad364e35~amd64~~10.0.22621.1.mum Z:\Install\Windows\servicing\Packages\Microsoft-Windows-OneDrive-Setup-WOW64-Package~31bf3856ad364e35~amd64~ru-RU~10.0.22621.1.cat Z:\Install\Windows\servicing\Packages\Microsoft-Windows-OneDrive-Setup-WOW64-Package~31bf3856ad364e35~amd64~ru-RU~10.0.22621.1.mum Z:\Install\Windows\System32\CatRoot\{F750E6C3-38EE-11D1-85E5-00C04FC295EE}\Microsoft-Windows-OneDrive-Setup-Package~31bf3856ad364e35~amd64~~10.0.22621.1.cat Z:\Install\Windows\System32\CatRoot\{F750E6C3-38EE-11D1-85E5-00C04FC295EE}\Microsoft-Windows-OneDrive-Setup-Package~31bf3856ad364e35~amd64~ru-RU~10.0.22621.1.cat Z:\Install\Windows\System32\CatRoot\{F750E6C3-38EE-11D1-85E5-00C04FC295EE}\Microsoft-Windows-OneDrive-Setup-WOW64-Package~31bf3856ad364e35~amd64~~10.0.22621.1.cat Z:\Install\Windows\System32\CatRoot\{F750E6C3-38EE-11D1-85E5-00C04FC295EE}\Microsoft-Windows-OneDrive-Setup-WOW64-Package~31bf3856ad364e35~amd64~ru-RU~10.0.22621.1.cat
+for %%a in (%DELETELIST%) do (
+	takeown /f %%a
+	icacls %%a /grant "%username%":f /c /l /q
+	del /f /q %%a
+)
+title UPFC
+set DEL=Z:\Install\Windows\System32\upfc.exe
+takeown /f %DEL%
+icacls %DEL% /grant "%username%":f /c /l /q
+del /f /q %DEL%
+move Z:\upfc.exe Z:\Install\Windows\System32
+%windir%\System32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy remotesigned -Command "& Get-Acl -Path Z:\Install\Windows\System32\control.exe | Set-Acl -Path %DEL%"
+set DEL=Z:\Install\Windows\WinSxS\amd64_microsoft-windows-upfc_31bf3856ad364e35_10.0.22621.1_none_08bb51571021559f
+takeown /f %DEL%
+icacls %DEL% /grant "%username%":f /c /l /q
+rd /s /q %DEL%
+set DEL=Z:\Install\Windows\WinSxS\Manifests\amd64_microsoft-windows-upfc_31bf3856ad364e35_10.0.22621.1_none_08bb51571021559f.manifest
+takeown /f %DEL%
+icacls %DEL% /grant "%username%":f /c /l /q
+del /f /q %DEL%
+title Calc
+set DELETELIST=Z:\Install\Windows\WinSxS\amd64_microsoft-windows-calc_31bf3856ad364e35_10.0.22621.1_none_0b53ccef0e7a283c Z:\Install\Windows\WinSxS\wow64_microsoft-windows-calc_31bf3856ad364e35_10.0.22621.1_none_15a8774142daea37
+for %%a in (%DELETELIST%) do (
+	takeown /f %%a
+	icacls %%a /grant "%username%":f /c /l /q
+	rd /s /q %%a
+)
+set DELETELIST=Z:\Install\Windows\System32\calc.exe Z:\Install\Windows\SysWOW64\calc.exe Z:\Install\Windows\WinSxS\Manifests\amd64_microsoft-windows-calc_31bf3856ad364e35_10.0.22621.1_none_0b53ccef0e7a283c.manifest Z:\Install\Windows\WinSxS\Manifests\wow64_microsoft-windows-calc_31bf3856ad364e35_10.0.22621.1_none_15a8774142daea37.manifest
+for %%a in (%DELETELIST%) do (
+	takeown /f %%a
+	icacls %%a /grant "%username%":f /c /l /q
+	del /f /q %%a
+)
+move Z:\Calc\calc.exe Z:\Install\Windows\System32
+%windir%\System32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy remotesigned -Command "& Get-Acl -Path Z:\Install\Windows\System32\control.exe | Set-Acl -Path Z:\Install\Windows\System32\calc.exe"
+move Z:\Calc\calc_64.exe Z:\Install\Windows\SysWOW64\calc.exe
+%windir%\System32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy remotesigned -Command "& Get-Acl -Path Z:\Install\Windows\System32\control.exe | Set-Acl -Path Z:\Install\Windows\SysWOW64\calc.exe"
+if exist Z:\Install\Windows\en-US\explorer.exe.mui (
+	Z:\PostClear\superUser64.exe /wrs %windir%\System32\cmd.exe /C copy Z:\Calc\calc.exe.en.mui Z:\Install\Windows\System32\en-US\calc.exe.mui
+	move Z:\Calc\calc.exe.en.mui Z:\Install\Windows\SysWOW64\en-US
+	move Z:\WinMine\en.exe Z:\PostClear\WinMine.exe
+)
+if exist Z:\Install\Windows\zh-CN\explorer.exe.mui (
+	Z:\PostClear\superUser64.exe /wrs %windir%\System32\cmd.exe /C copy Z:\Calc\calc.exe.cn.mui Z:\Install\Windows\System32\zh-CN\calc.exe.mui
+	move Z:\Calc\calc.exe.cn.mui Z:\Install\Windows\SysWOW64\zh-CN
+	move Z:\WinMine\cn.exe Z:\PostClear\WinMine.exe
+)
+if exist Z:\Install\Windows\ru-RU\explorer.exe.mui (
+	Z:\PostClear\superUser64.exe /wrs %windir%\System32\cmd.exe /C copy Z:\Calc\calc.exe.ru.mui Z:\Install\Windows\System32\ru-RU\calc.exe.mui
+	move Z:\Calc\calc.exe.ru.mui Z:\Install\Windows\SysWOW64\ru-RU
+	move Z:\WinMine\ru.exe Z:\PostClear\WinMine.exe
+)
+rd /s /q Z:\Calc
+rd /s /q Z:\WinMine
+title GameDVR
+set DEL="Z:\Install\Windows\bcastdvr\KnownGameList.bin"
+takeown /f %DEL%
+icacls %DEL% /grant "%username%":f /c /l /q
+del /f /q %DEL%
+move Z:\KnownGameList.bin %DEL%
+%windir%\System32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy remotesigned -Command "& Get-Acl -Path Z:\Install\Windows\System32\control.exe | Set-Acl -Path %DEL%"
+title WaaS tasks
+set DELETELIST=Z:\Install\Windows\WaaS\regkeys Z:\Install\Windows\WaaS\services Z:\Install\Windows\WaaS
+for %%a in (%DELETELIST%) do (
+	takeown /f %%a
+	icacls %%a /grant "%username%":f /c /l /q
+	rd /s /q %%a
+)
+set DELETELIST=Z:\Install\Windows\WinSxS\FileMaps\$$_waas_regkeys_dbffc348a6fab71c.cdf-ms Z:\Install\Windows\WinSxS\FileMaps\$$_waas_services_ddfc4ae175ff1678.cdf-ms
+for %%a in (%DELETELIST%) do (
+	takeown /f %%a
+	icacls %%a /grant "%username%":f /c /l /q
+	del /f /q %%a
+)
+title Clear WinSxS
+for /f "tokens=*" %%i in ('dir Z:\Install\Windows\WinSxS\Backup /b /a:-d') do (
+	icacls "Z:\Install\Windows\WinSxS\Backup\%%~i" /grant "%username%":f /c /l /q
+	del /f /q "Z:\Install\Windows\WinSxS\Backup\%%~i"
+)
+title Compress Winre
+Z:\WimOptimize.exe Z:\Install\Windows\System32\Recovery\Winre.wim
+del /f /q Z:\WimOptimize.exe
+TIMEOUT /T 1 /NOBREAK >nul
+title Copy PostClear
+if not exist Z:\Install\Windows\ru-RU\explorer.exe.mui (
+	del /f /q Z:\PostClear\WinHelp.html
+)
+move Z:\PostClear Z:\Install\ProgramData\PostClear
+title Unmounting
+dism /unmount-wim /mountdir:Z:\Install /commit
+title Done...
+del /f /q Z:\Windows-11-DISM-Scripts.rar
+del /f /q Z:\_Clear.bat
